61879717c9c54ce14d2e20d31d8a7d3299176c2c
confimex <adolfoms yahoo.com>
2026-08-20 11:47:19 -0600

    Agregar WhatsApp Business global a CONFIMEX

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { Outlet, Link, createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
-
+import FloatingWhatsApp from "../components/FloatingWhatsApp";
 import appCss from "../styles.css?url";
 
 function NotFoundComponent() {
@@ -67,5 +67,10 @@ function RootShell({ children }: { children: React.ReactNode }) {
 }
 
 function RootComponent() {
-  return <Outlet />;
+  return (
+    <>
+      <Outlet />
+      <FloatingWhatsApp />
+    </>
+  );
 }
